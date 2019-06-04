1742169fa34572e4706db1f7f37a9598d712816f
boanergepro <antonycarrizo96@gmail.com>
2019-06-03 22:40:01 -0400

    [MOD]: - Cambiada apariencia del login.

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scancode_app/src/widgets/custom_textfield.dart';
 
 class LoginScreen extends StatefulWidget {
   static String routerName = '/';
@@ -17,22 +18,96 @@ class _LoginScreenState extends State<LoginScreen> {
           Expanded(
             flex: 2,
             child: Container(
-                color: Theme.of(context).primaryColor,
-                child: Center(
-                  child: Text(
-                    '//ScanCode',
-                    style: TextStyle(
-                      fontFamily: 'BalooDa',
-                      fontSize: 35,
-                      color: Colors.white,
-                    ),
+              color: Theme.of(context).primaryColor,
+              child: Center(
+                child: Text(
+                  '//ScanCode',
+                  style: TextStyle(
+                    fontFamily: 'BalooDa',
+                    fontSize: 35,
+                    color: Colors.white,
                   ),
-                )),
+                ),
+              ),
+            ),
           ),
           Expanded(
             flex: 3,
             child: Container(
-              color: Colors.white,
+              child: Column(
+                children: <Widget>[
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Expanded(
+                      flex: 4,
+                      child: Row(
+                        children: <Widget>[
+                          Spacer(
+                            flex: 2,
+                          ),
+                          Expanded(
+                            flex: 8,
+                            child: Column(
+                              children: <Widget>[
+                                // Texxfiel
+                                Expanded(
+                                  flex: 3,
+                                  child: customTextField(
+                                    labelText: 'username',
+                                    prefixIcon: Icons.alternate_email,
+                                  ),
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Expanded(
+                                  flex: 5,
+                                  child: Container(
+                                    child: Text(
+                                      'Ingresa tu username y descubre como el mundo podria ver lo que te define "Tu codigo" ',
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(
+                                        fontFamily: 'BalooDa',
+                                        fontSize: 15,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Spacer(
+                            flex: 2,
+                          ),
+                        ],
+                      )),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      //color: Theme.of(context).primaryColor,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Buscar',
+                          style: TextStyle(
+                            fontFamily: 'BalooDa',
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacer(
+                    flex: 1,
+                  )
+                ],
+              ),
             ),
           )
         ],
