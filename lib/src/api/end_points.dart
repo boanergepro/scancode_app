2eda84353e970ce7f4207d2efa8a271bfbcd9abd
boanergepro <antonycarrizo96@gmail.com>
2019-06-07 12:53:44 -0400

    [ADD]: - Agregados end points del api de codetrace.

---
+++ lib/src/api/end_points.dart
@@ -0,0 +1,3 @@
+const URL_USER = 'https://codetrace.com/api/users/login/';
+const URL_BADGES = 'https://codetrace.com/static/images/badges/';
+const URL_LANGUAGES = 'https://codetrace.com/static/images/languages/';
