5b111ff92b631b98fee2051cd6f8be69d817536f
boanergepro <antonycarrizo96@gmail.com>
2019-06-07 15:37:02 -0400

    [FIX]: - Corregido error al renderizar ek teclado.

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -6,9 +6,16 @@ import 'package:scancode_app/src/providers/user.dart';
 import 'package:scancode_app/src/providers/app.dart';
 import 'package:scancode_app/src/widgets/loading_overlay.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static final routerName = '/';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   final TextEditingController _usernameController = TextEditingController();
 
   @override
@@ -20,116 +27,114 @@ class LoginScreen extends StatelessWidget {
       key: _scaffoldKey,
       body: Stack(
         children: <Widget>[
-          ListView(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height,
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        color: Theme.of(context).primaryColor,
-                        child: Center(
-                          child: Text(
-                            '//ScanCode',
-                            style: TextStyle(
-                              fontFamily: 'BalooDa',
-                              fontSize: 35,
-                              color: Colors.white,
-                            ),
+          SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Theme.of(context).primaryColor,
+                      child: Center(
+                        child: Text(
+                          '//ScanCode',
+                          style: TextStyle(
+                            fontFamily: 'BalooDa',
+                            fontSize: 35,
+                            color: Colors.white,
                           ),
                         ),
                       ),
                     ),
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Spacer(
-                              flex: 1,
-                            ),
-                            Expanded(
-                              flex: 4,
-                              child: Row(
-                                children: <Widget>[
-                                  Spacer(
-                                    flex: 2,
-                                  ),
-                                  Expanded(
-                                    flex: 8,
-                                    child: Column(
-                                      children: <Widget>[
-                                        // Texxfiel
-                                        Expanded(
-                                          flex: 3,
-                                          child: customTextField(
-                                            controller: _usernameController,
-                                            labelText: 'username',
-                                            prefixIcon: Icons.alternate_email,
-                                          ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          Spacer(
+                            flex: 1,
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Row(
+                              children: <Widget>[
+                                Spacer(
+                                  flex: 2,
+                                ),
+                                Expanded(
+                                  flex: 8,
+                                  child: Column(
+                                    children: <Widget>[
+                                      // Texxfiel
+                                      Expanded(
+                                        flex: 3,
+                                        child: customTextField(
+                                          controller: _usernameController,
+                                          labelText: 'username',
+                                          prefixIcon: Icons.alternate_email,
                                         ),
-                                        Spacer(
-                                          flex: 1,
-                                        ),
-                                        Expanded(
-                                          flex: 5,
-                                          child: Container(
-                                            child: Text(
-                                              'Ingresa tu username y descubre como el mundo podria ver lo que te define "Tu codigo" ',
-                                              textAlign: TextAlign.justify,
-                                              style: TextStyle(
-                                                fontFamily: 'BalooDa',
-                                                fontSize: 15,
-                                                color: Colors.grey,
-                                              ),
+                                      ),
+                                      Spacer(
+                                        flex: 1,
+                                      ),
+                                      Expanded(
+                                        flex: 5,
+                                        child: Container(
+                                          child: Text(
+                                            'Enter your username and discover how the world could see what defines you "Your code" ',
+                                            textAlign: TextAlign.justify,
+                                            style: TextStyle(
+                                              fontFamily: 'BalooDa',
+                                              fontSize: 15,
+                                              color: Colors.grey,
                                             ),
                                           ),
-                                        )
-                                      ],
-                                    ),
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                  Spacer(
-                                    flex: 2,
-                                  ),
-                                ],
-                              ),
+                                ),
+                                Spacer(
+                                  flex: 2,
+                                ),
+                              ],
                             ),
-                            Spacer(
-                              flex: 1,
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: Container(
-                                width: double.infinity,
-                                color: Theme.of(context).primaryColor,
-                                child: RaisedButton(
-                                  onPressed: () {
-                                    _handleLogin(context, appState, userState);
-                                  },
-                                  child: Text(
-                                    'Buscar',
-                                    style: TextStyle(
-                                      fontFamily: 'BalooDa',
-                                      fontSize: 20,
-                                      color: Colors.white,
-                                    ),
+                          ),
+                          Spacer(
+                            flex: 1,
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              width: double.infinity,
+                              color: Theme.of(context).primaryColor,
+                              child: RaisedButton(
+                                onPressed: () {
+                                  _handleLogin(context, appState, userState);
+                                },
+                                child: Text(
+                                  'Search',
+                                  style: TextStyle(
+                                    fontFamily: 'BalooDa',
+                                    fontSize: 20,
+                                    color: Colors.white,
                                   ),
                                 ),
                               ),
                             ),
-                            Spacer(
-                              flex: 1,
-                            )
-                          ],
-                        ),
+                          ),
+                          Spacer(
+                            flex: 1,
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
               ),
-            ],
+            ),
           ),
           LoadingOverlay(
             loading: appState.loadingOverlay,
@@ -153,7 +158,7 @@ class LoginScreen extends StatelessWidget {
       _scaffoldKey.currentState.showSnackBar(
         SnackBar(
           content: Text(
-              'Error! Por favor verifique el nobre de usurio o su conexion a internet'),
+              'Error! Please verify the username or your internet connection'),
         ),
       );
       throw Exception('Failed to load User');
