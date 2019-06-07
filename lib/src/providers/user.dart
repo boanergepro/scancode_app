adbd62a916bfcfcf90c570cac43e7fb2303e9be1
boanergepro <antonycarrizo96@gmail.com>
2019-06-07 12:52:33 -0400

    [MOD]: - Modificada forma de usar la url de pa peticion

--- lib/src/providers/user.dart
+++ lib/src/providers/user.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:scancode_app/src/models/user.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:scancode_app/src/api/end_points.dart';
 
 class UserProvider with ChangeNotifier {
   User _user;
@@ -9,8 +10,7 @@ class UserProvider with ChangeNotifier {
   User get user => _user;
 
   Future<int> loadDataUser(String username) async {
-    final response = await http.get(
-        'https://codetrace.com/api/users/login/$username');
+    final response = await http.get('$URL_USER$username');
     switch (response.statusCode) {
       case 200:
         _user = User.fromJson(json.decode(response.body));
