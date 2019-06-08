fb434d7e8d2161bde9c4191c006ad3ef485a0af1
alirio gutierrez <alirio1925@gmail.com>
2019-06-07 21:10:07 -0400

    [ADD] se agrego route /leaders

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:scancode_app/src/screens/login_screen.dart';
 import 'package:scancode_app/src/screens/home_screen.dart';
+import 'package:scancode_app/src/screens/leaders_screen.dart';
 
 final routes = {
   LoginScreen.routerName: (BuildContext context) => LoginScreen(),
   HomeScreen.routerName: (BuildContext context) => HomeScreen(),
+  LeadersScreen.routerName: (BuildContext context) => LeadersScreen(),
 };
