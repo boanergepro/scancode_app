45118f37be7ff7543d6f67e306e73daa2e01fdcd
alirio gutierrez <alirio1925@gmail.com>
2019-06-10 23:35:54 -0400

    [MOD] se modifico parametro del constructor

--- lib/src/models/leader.dart
+++ lib/src/models/leader.dart
@@ -1,32 +1,40 @@
-import 'package:scancode_app/src/providers/leaders.dart';
+import 'package:scancode_app/src/providers/leader.dart';
 import 'package:http/http.dart' as http;
 import 'package:scancode_app/src/api/end_points.dart';
 
-class Leaders{
+class Leader {
+  String _id;
   String _name;
   String _username;
   String _avatarUrl;
   String _location;
-  Map<String,dynamic> _experience;
+  Map<String, dynamic> _experience;
   String _countryCode;
 
   // Constructor
-  Leaders.fromJson(List<dynamic> json){
-    json.map((item) => Leaders.fromJson(item)).toList();
-  }
+  Leader.fromJson(Map<dynamic, dynamic> json) {
+    _id = json['_id'] ?? null;
+    _name = json['name'] ?? "";
+    _username = json['login'] ?? "";
+    _avatarUrl = json['avatarUrl'] ?? "";
+    _location = json['location'] ?? "";
+    _experience = json['experience'] ?? "";
+    _countryCode = json['countryCode'] ?? "";
 
-  loadData() async{
-    final response = await http.get(URL_LEADERS);
-    return response.body;
   }
 
   // Getter
+  String get id => _id;
+
   String get name => _name;
+
   String get username => _username;
+
   String get avatarUrl => _avatarUrl;
+
   String get location => _location;
-  Map<String,dynamic> get experience => _experience;
+
+  Map<String, dynamic> get experience => _experience;
+
   String get countryCode => _countryCode;
-
-
 }
