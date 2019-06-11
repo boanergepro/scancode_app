beee8bc157ebb0729951110a52976be974532f3d
boanergepro <antonycarrizo96@gmail.com>
2019-06-11 11:20:18 -0400

    [MOD]: Modularize la seccion de las insignias.

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:scancode_app/src/widgets/home_header.dart';
 import 'package:scancode_app/src/providers/user.dart';
-import 'package:scancode_app/src/widgets/item_list_badges.dart';
+import 'package:scancode_app/src/widgets/home_badges.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routerName = '/home';
@@ -63,51 +63,7 @@ class HomeScreen extends StatelessWidget {
                 <Widget>[
                   Container(
                     height: _screenHeight / 5,
-                    child: Column(
-                      children: <Widget>[
-                        Flexible(
-                          flex: 2,
-                          child: Row(
-                            children: <Widget>[
-                              Spacer(
-                                flex: 1,
-                              ),
-                              Expanded(
-                                flex: 15,
-                                child: Text(
-                                  'Overview',
-                                  style: TextStyle(
-                                    fontFamily: 'BalooDa',
-                                    fontSize: 18,
-                                    color: Colors.black87,
-                                  ),
-                                ),
-                              ),
-                              Spacer(
-                                flex: 1,
-                              )
-                            ],
-                          ),
-                        ),
-                        //List Badges
-                        Expanded(
-                          flex: 6,
-                          child: LayoutBuilder(
-                            builder: (context, constraints) {
-                              return Container(
-                                child: ListView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: userState.user.badges.length,
-                                  itemBuilder: (context, index) {
-                                    return itemListBadges(constraints, userState, index);
-                                  },
-                                ),
-                              );
-                            },
-                          ),
-                        )
-                      ],
-                    ),
+                    child: homeBadges(userState),
                   ),
                 ],
               ),
