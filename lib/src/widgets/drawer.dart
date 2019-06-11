fa1e2755bcee16933081f104c0de7aa571846730
alirio gutierrez <alirio1925@gmail.com>
2019-06-10 23:36:47 -0400

    [ADD] se agrego loading a leaders en el drower

--- lib/src/widgets/drawer.dart
+++ lib/src/widgets/drawer.dart
@@ -1,31 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:scancode_app/src/providers/app.dart';
 import 'package:scancode_app/src/screens/leaders_screen.dart';
+import 'package:scancode_app/src/providers/leader.dart';
+import 'package:scancode_app/src/widgets/loading_overlay.dart';
 
 Widget drawer(BuildContext context){
+  final leadersState = Provider.of<LeaderProvider>(context);
+  final appState = Provider.of<AppProvider>(context);
   return Drawer(
-    child: ListView(
+    child: Stack(
       children: <Widget>[
-        DrawerHeader(
-          child: Text('//ScanCode',style: TextStyle(
-            fontFamily: 'BalooDa',
-            fontSize: 30,
-            color: Colors.white,
-          ),),
-          decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-          ),
+        ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('//ScanCode',style: TextStyle(
+                fontFamily: 'BalooDa',
+                fontSize: 30,
+                color: Colors.white,
+              ),),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            ListTile(
+              title: Text('Leaders',style: TextStyle(
+                fontFamily: 'BalooDa',
+                fontSize: 15,
+              )),
+              onTap: () {
+                _getLeaders(appState, leadersState, context);
+              },
+            ),
+          ],
         ),
-        ListTile(
-          title: Text('Leaders',style: TextStyle(
-            fontFamily: 'BalooDa',
-            fontSize: 15,
-          )),
-          onTap: () {
-            Navigator.pushNamed(context, LeadersScreen.routerName);
-            // ...
-          },
+        LoadingOverlay(
+          loading: appState.loadingOverlay,
         )
       ],
-    ),
+    )
   );
+}
+
+void _getLeaders(AppProvider appState, LeaderProvider leadersState, BuildContext context) async {
+  appState.loadingOverlay = true;
+
+  var responseCode = await leadersState.loadDataLeaders();
+
+  if (responseCode == 200) {
+    appState.loadingOverlay = false;
+    Navigator.pushNamed(context, LeadersScreen.routerName);
+  } else {
+    appState.loadingOverlay = false;
+  }
 }
