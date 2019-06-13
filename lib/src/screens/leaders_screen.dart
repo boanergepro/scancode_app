b10424e8b480aeb0c480e0a3e351a9e9c6406be0
alirio gutierrez <alirio1925@gmail.com>
2019-06-13 13:57:20 -0400

    [MOD] se refactorizo la vista del widget

--- lib/src/screens/leaders_screen.dart
+++ lib/src/screens/leaders_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:scancode_app/src/providers/leader.dart';
 import 'package:scancode_app/src/widgets/drawer.dart';
-import 'package:scancode_app/src/widgets/drawer.dart';
 
 class LeadersScreen extends StatelessWidget {
   static final routerName = '/leaders';
@@ -12,133 +11,177 @@ class LeadersScreen extends StatelessWidget {
     final leaderState = Provider.of<LeaderProvider>(context);
 
     return Scaffold(
+      backgroundColor: Colors.purple,
       appBar: AppBar(
         title: Text('Leaders'),
-      ),drawer: drawer(context) ,
+      ),
+      drawer: drawer(context),
       body: ListView.builder(
         itemCount: leaderState.leaders.length,
         itemBuilder: (context, int index) {
-          return Card(
-              color: Colors.purple,
-              child: Container(
-                height: 140,
-                child: Row(
-                  children: <Widget>[
-                    Spacer(
-                      flex: 1,
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: CircleAvatar(
-                        radius: 15,
-                        backgroundColor: Colors.purple[200],
-                        child: Text('${index + 1}',
-                            style: TextStyle(
-                              fontFamily: 'BalooDa',
-                              color: Colors.white,
+          return Container(
+            padding: EdgeInsets.only(right: 10, left: 10, top: 5, bottom: 5),
+            child: Card(
+                color: Colors.white,
+                child: InkWell(
+                  onTap: () {
+
+                  },
+                  child: Container(
+                    height: 160,
+                    child: Row(
+                      children: <Widget>[
+                        Spacer(
+                          flex: 1,
+                        ),
+                        Expanded(
+                            flex: 6,
+                            child: Container(
+                              child: Column(
+                                children: <Widget>[
+                                  Spacer(
+                                    flex: 1,
+                                  ),
+                                  Expanded(
+                                    flex: 8,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.purple, width: 2),
+                                        borderRadius: BorderRadius.circular(10),
+                                        image: DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: NetworkImage(leaderState
+                                              .leaders[index].avatarUrl),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Spacer(
+                                    flex: 1,
+                                  )
+                                ],
+                              ),
                             )),
-                      ),
-                    ),
-                    Spacer(
-                      flex: 1,
-                    ),
-                    Expanded(
-                        flex: 6,
-                        child: Container(
+                        Spacer(),
+                        Expanded(
+                            flex: 12,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Spacer(flex: 1),
+                                Text(leaderState.leaders[index].name,
+                                    style: TextStyle(
+                                      fontFamily: 'BalooDa',
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                    )),
+                                Row(
+                                  children: <Widget>[
+                                    Flexible(
+                                      flex: 2,
+                                      child:
+                                          Icon(Icons.place, color: Colors.purple),
+                                    ),
+                                    Spacer(
+                                      flex: 1,
+                                    ),
+                                    Expanded(
+                                      flex: 12,
+                                      child: Text(
+                                        leaderState.leaders[index].location,
+                                        textScaleFactor: 0.8,
+                                        style: TextStyle(
+                                          fontFamily: 'BalooDa',
+                                          fontSize: 15,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Flexible(
+                                      flex: 2,
+                                      child: Icon(Icons.multiline_chart,
+                                          color: Colors.purple),
+                                    ),
+                                    Spacer(
+                                      flex: 1,
+                                    ),
+                                    Expanded(
+                                      flex: 12,
+                                      child: Text(
+                                        _getLevel(leaderState
+                                            .leaders[index].experience['total']),
+                                        textScaleFactor: 0.8,
+                                        style: TextStyle(
+                                          fontFamily: 'BalooDa',
+                                          fontSize: 15,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Flexible(
+                                      flex: 2,
+                                      child: Icon(Icons.alternate_email,
+                                          color: Colors.purple),
+                                    ),
+                                    Spacer(
+                                      flex: 1,
+                                    ),
+                                    Expanded(
+                                      flex: 12,
+                                      child: Text(
+                                        leaderState.leaders[index].username,
+                                        textScaleFactor: 0.8,
+                                        style: TextStyle(
+                                          fontFamily: 'BalooDa',
+                                          fontSize: 15,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                              ],
+                            )),
+                        Spacer(flex: 1,),
+                        Expanded(
+                          flex: 2,
                           child: Column(
                             children: <Widget>[
-                              Spacer(
-                                flex: 1,
-                              ),
-                              Expanded(
-                                flex: 8,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                        color: Colors.purple[200], width: 2),
-                                    borderRadius: BorderRadius.circular(10),
-                                    image: DecorationImage(
-                                      fit: BoxFit.fill,
-                                      image: NetworkImage(
-                                          leaderState.leaders[index].avatarUrl),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Spacer(
-                                flex: 1,
-                              )
+                            Spacer(flex: 1,),
+                              Expanded(flex: 2,
+                              child: CircleAvatar(
+                                radius: 15,
+                                backgroundColor: Colors.purple,
+                                child: Text('${index + 1}',
+                                    style: TextStyle(
+                                      fontFamily: 'BalooDa',
+                                      color: Colors.white,
+                                    )),
+                              ),),
+                              Spacer(flex: 7)
                             ],
-                          ),
-                        )),
-                    Spacer(),
-                    Expanded(
-                        flex: 12,
-                        child: Column(
-                          children: <Widget>[
-                            Spacer(flex: 1),
-                            Text(leaderState.leaders[index].name,
-                                style: TextStyle(
-                                  fontFamily: 'BalooDa',
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                )),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Flexible(
-                                  flex: 2,
-                                  child: Icon(Icons.place),
-                                ),
-                                Spacer(
-                                  flex: 1,
-                                ),
-                                Expanded(
-                                  flex: 5,
-                                  child: Text(
-                                    leaderState.leaders[index].location,
-                                    textScaleFactor: 0.8,
-                                    style: TextStyle(
-                                      fontFamily: 'BalooDa',
-                                      fontSize: 15,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Text(''),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Flexible(
-                                  flex: 2,
-                                  child: Icon(Icons.multiline_chart),
-                                ),
-                                Spacer(
-                                  flex: 1,
-                                ),
-                                Expanded(
-                                  flex: 5,
-                                  child: Text(
-                                    _getLevel(leaderState
-                                        .leaders[index].experience['total']),
-                                    textScaleFactor: 0.8,
-                                    style: TextStyle(
-                                      fontFamily: 'BalooDa',
-                                      fontSize: 15,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Spacer(flex: 1,)
-                          ],
-                        ))
-                  ],
-                ),
-              ));
+                          )
+                        ),
+                        Spacer(
+                          flex: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+          );
         },
       ),
     );
