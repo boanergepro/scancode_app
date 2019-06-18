2a676ce77b7653e1413a76b4256cc53f6da6cd97
alirio gutierrez <alirio1925@gmail.com>
2019-06-18 11:22:40 -0400

    [ADD] se agrego widget languagecode

---
+++ lib/src/widgets/dropdown_language_code.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+List<DropdownMenuItem> listLanguageCode = [
+  DropdownMenuItem(
+    child: Text('C'),
+    value: 'c',
+  ),
+  DropdownMenuItem(
+    child: Text('C##'),
+    value: 'csharp',
+  ),
+  DropdownMenuItem(
+    child: Text('C++'),
+    value: 'cpp',
+  ),
+  DropdownMenuItem(
+    child: Text('Elixir'),
+    value: 'elixir',
+  ),
+  DropdownMenuItem(
+    child: Text('Go'),
+    value: 'go',
+  ),
+  DropdownMenuItem(
+    child: Text('Java'),
+    value: 'java',
+  ),
+  DropdownMenuItem(
+    child: Text('Javascript'),
+    value: 'javascript',
+  ),
+  DropdownMenuItem(
+    child: Text('Lua'),
+    value: 'lua',
+  ),
+  DropdownMenuItem(
+    child: Text('Objetive C'),
+    value: 'objective-c',
+  ),
+  DropdownMenuItem(
+    child: Text('PHP'),
+    value: 'php',
+  ),
+  DropdownMenuItem(
+    child: Text('Python'),
+    value: 'python',
+  ),
+  DropdownMenuItem(
+    child: Text('R'),
+    value: 'r',
+  ),
+  DropdownMenuItem(
+    child: Text('Ruby'),
+    value: 'ruby',
+  ),
+  DropdownMenuItem(
+    child: Text('Rust'),
+    value: 'rust',
+  ),
+  DropdownMenuItem(
+    child: Text('Scala'),
+    value: 'scala',
+  ),
+  DropdownMenuItem(
+    child: Text('Swift'),
+    value: 'swift',
+  ),
+  DropdownMenuItem(
+    child: Text('Typescript'),
+    value: 'typescript',
+  )
+];
