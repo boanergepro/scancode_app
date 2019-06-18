ec3175baf7ffe7b231eab89aab6dbe8702720ffe
alirio gutierrez <alirio1925@gmail.com>
2019-06-18 11:22:15 -0400

    [ADD] se agrego widget countrycode

---
+++ lib/src/widgets/dropdown_country_code.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+
+List<DropdownMenuItem> listCountryCode = [
+  DropdownMenuItem(
+    child: Text('Argentina'),
+    value: 'ar',
+  ),
+  DropdownMenuItem(
+    child: Text('Armenia'),
+    value: 'am',
+  ),
+  DropdownMenuItem(
+    child: Text('Australia'),
+    value: 'au',
+  ),
+  DropdownMenuItem(
+    child: Text('Bangladesh'),
+    value: 'bd',
+  ),
+  DropdownMenuItem(
+    child: Text('Belarus'),
+    value: 'by',
+  ),
+  DropdownMenuItem(
+    child: Text('Bolivia'),
+    value: 'bo',
+  ),
+  DropdownMenuItem(
+    child: Text('Brazil'),
+    value: 'br',
+  ),
+  DropdownMenuItem(
+    child: Text('Canada'),
+    value: 'ca',
+  ),
+  DropdownMenuItem(
+    child: Text('Chile'),
+    value: 'cl',
+  ),
+  DropdownMenuItem(
+    child: Text('China'),
+    value: 'cn',
+  ),
+  DropdownMenuItem(
+    child: Text('Colombia'),
+    value: 'co',
+  ),
+  DropdownMenuItem(
+    child: Text('Denmark'),
+    value: 'dk',
+  ),
+  DropdownMenuItem(
+    child: Text('Ecuador'),
+    value: 'ec',
+  ),
+  DropdownMenuItem(
+    child: Text('Egypt'),
+    value: 'eg',
+  ),
+  DropdownMenuItem(
+    child: Text('France'),
+    value: 'fr',
+  ),
+  DropdownMenuItem(
+    child: Text('Germany'),
+    value: 'de',
+  ),
+  DropdownMenuItem(
+    child: Text('Honduras'),
+    value: 'hn',
+  ),
+  DropdownMenuItem(
+    child: Text('Hong Kong'),
+    value: 'hk',
+  ),
+  DropdownMenuItem(
+    child: Text('India'),
+    value: 'in',
+  ),
+  DropdownMenuItem(
+    child: Text('Indonesia'),
+    value: 'id',
+  ),
+  DropdownMenuItem(
+    child: Text('Iran'),
+    value: 'ir',
+  ),
+  DropdownMenuItem(
+    child: Text('Italy'),
+    value: 'it',
+  ),
+  DropdownMenuItem(
+    child: Text('Malaysia'),
+    value: 'my',
+  ),
+  DropdownMenuItem(
+    child: Text('Mexico'),
+    value: 'mx',
+  ),
+  DropdownMenuItem(
+    child: Text('Nepal'),
+    value: 'np',
+  ),
+  DropdownMenuItem(
+    child: Text('Nigeria'),
+    value: 'ng',
+  ),
+  DropdownMenuItem(
+    child: Text('Pakistan'),
+    value: 'pk',
+  ),
+  DropdownMenuItem(
+    child: Text('Peru'),
+    value: 'pe',
+  ),
+  DropdownMenuItem(
+    child: Text('Philippines'),
+    value: 'ph',
+  ),
+  DropdownMenuItem(
+    child: Text('Poland'),
+    value: 'pl',
+  ),
+  DropdownMenuItem(
+    child: Text('Portugal'),
+    value: 'pt',
+  ),
+  DropdownMenuItem(
+    child: Text('Russia'),
+    value: 'ru',
+  ),
+  DropdownMenuItem(
+    child: Text('Singapore'),
+    value: 'sg',
+  ),
+  DropdownMenuItem(
+    child: Text('South Korea'),
+    value: 'kr',
+  ),
+  DropdownMenuItem(
+    child: Text('Spain'),
+    value: 'es',
+  ),
+  DropdownMenuItem(
+    child: Text('Sri Lanka'),
+    value: 'lk',
+  ),
+  DropdownMenuItem(
+    child: Text('Taiwan'),
+    value: 'tw',
+  ),
+  DropdownMenuItem(
+    child: Text('Thailand'),
+    value: 'th',
+  ),
+  DropdownMenuItem(
+    child: Text('United Kingdom'),
+    value: 'gb',
+  ),
+  DropdownMenuItem(
+    child: Text('United States'),
+    value: 'us',
+  ),
+  DropdownMenuItem(
+    child: Text('Uruguay'),
+    value: 'uy',
+  ),
+  DropdownMenuItem(
+    child: Text('Venezuela'),
+    value: 've',
+  )
+];
