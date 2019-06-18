f1471ddb6bcdff39da7877ae9916f17eab679410
alirio gutierrez <alirio1925@gmail.com>
2019-06-18 13:03:06 -0400

    [MOD] se modifico estructura del widget

--- lib/src/widgets/bottom_sheet_filter_leaders.dart
+++ lib/src/widgets/bottom_sheet_filter_leaders.dart
@@ -10,18 +10,31 @@ class ModalBottomSheet extends StatefulWidget {
 
 class _ModalBottomSheetState extends State<ModalBottomSheet>
     with SingleTickerProviderStateMixin {
-
   Widget build(BuildContext context) {
     final leaderState = Provider.of<LeaderProvider>(context);
     return Container(
-      height: 200,
+      height: 180,
       child: Column(
         children: <Widget>[
           Spacer(
             flex: 1,
           ),
           Expanded(
-            flex: 5,
+            flex: 2,
+            child: Text(
+              'Select an option to filter',
+              style: TextStyle(
+                fontFamily: 'BalooDa',
+                fontSize: 15,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Spacer(
+            flex: 1,
+          ),
+          Expanded(
+            flex: 4,
             child: Row(
               children: <Widget>[
                 Spacer(
@@ -29,29 +42,51 @@ class _ModalBottomSheetState extends State<ModalBottomSheet>
                 ),
                 Expanded(
                   flex: 10,
-                  child: DropdownButton(
-                    hint: Text('All languages'),
-                    items: listLanguageCode,
-                    value: leaderState.languageCode,
-                    onChanged: (value) {
-                      leaderState.currentLanguageCode = value;
-                    },
-                  ),
+                  child: Container(
+                      padding: EdgeInsets.only(left: 10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(color: Colors.purple)),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                          hint: Text('All languages',
+                              style: TextStyle(
+                                fontFamily: 'BalooDa',
+                                fontSize: 15,
+                                color: Colors.black,
+                              )),
+                          items: listLanguageCode,
+                          value: leaderState.languageCode,
+                          onChanged: (value) {
+                            leaderState.currentLanguageCode = value;
+                          },
+                        ),
+                      )),
                 ),
                 Spacer(
                   flex: 1,
                 ),
                 Expanded(
                   flex: 10,
-                  child: DropdownButton(
-                    hint: Text('All countries'),
-                    items: listCountryCode,
-                    value: leaderState.countryCode,
-                    onChanged: (value) {
-                      print(leaderState.countryCode);
-                      print(value);
-                      leaderState.currentCountryCode = value;
-                    },
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.purple)),
+                    child: DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                      hint: Text('All countries',
+                          style: TextStyle(
+                            fontFamily: 'BalooDa',
+                            fontSize: 15,
+                            color: Colors.black,
+                          )),
+                      items: listCountryCode,
+                      value: leaderState.countryCode,
+                      onChanged: (value) {
+                        leaderState.currentCountryCode = value;
+                      },
+                    )),
                   ),
                 ),
                 Spacer(
@@ -64,10 +99,35 @@ class _ModalBottomSheetState extends State<ModalBottomSheet>
             flex: 1,
           ),
           Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.green,
+            flex: 4,
+            child: Row(
+              children: <Widget>[
+                Spacer(flex: 3),
+                Expanded(
+                  flex: 5,
+                  child: Center(
+                    child: RaisedButton(
+                      color: Colors.purple,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      child: Text(
+                        'Search',
+                        style: TextStyle(
+                          fontFamily: 'BalooDa',
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {}, //callback when button is clicked
+                    ),
+                  ),
+                ),
+                Spacer(flex: 3)
+              ],
             ),
+          ),
+          Spacer(
+            flex: 1,
           )
         ],
       ),
