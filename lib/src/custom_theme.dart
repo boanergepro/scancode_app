591a1df0344d078e59f437c28e1334dd5ede2493
boanergepro <antonycarrizo96@gmail.com>
2019-06-24 13:57:01 -0400

    [MOD]: - Elimine el color para los botones del tema.

--- lib/src/custom_theme.dart
+++ lib/src/custom_theme.dart
@@ -2,6 +2,5 @@ import 'package:flutter/material.dart';
 
 ThemeData customTheme = ThemeData(
   primaryColor: Colors.purple,
-  buttonColor: Colors.purple[300],
   iconTheme: IconThemeData(color: Colors.white),
 );
