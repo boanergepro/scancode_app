23a2024af480f4cc5576f03a89f81b2d993214cd
boanergepro <antonycarrizo96@gmail.com>
2019-06-24 14:06:36 -0400

    [MOD]: Elimine end point no usado.

--- lib/src/api/end_points.dart
+++ lib/src/api/end_points.dart
@@ -1,5 +1,4 @@
 const URL_USER = 'https://codetrace.com/api/users/login/';
 const URL_BADGES = 'https://codetrace.com/static/images/badges/';
 const URL_LANGUAGES = 'https://codetrace.com/static/images/languages/';
-const URL_LEADERS = 'https://codetrace.com/api/leaders';
 const URL_INTEGRATIONS = 'https://codetrace.com/static/images/integrations/';
