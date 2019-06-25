214f1710e4418fd187b81a49dd7389f6b9faca7d
boanergepro <antonycarrizo96@gmail.com>
2019-06-25 11:48:36 -0400

    [MOD]: - Agregado colores necesarios del tema.

--- lib/src/custom_theme.dart
+++ lib/src/custom_theme.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 ThemeData customTheme = ThemeData(
   primaryColor: Colors.purple,
   iconTheme: IconThemeData(color: Colors.white),
+  // Color de las sombras
+  accentColor: Colors.purple[300],
+  // Color de sombra al darle tap a un item con evento
+  splashColor: Colors.purple[300],
 );
