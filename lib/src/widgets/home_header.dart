152934b32867c975b3e99b21df3ba027f16280c6
boanergepro <antonycarrizo96@gmail.com>
2019-07-04 14:51:37 -0400

    [MOD]: Elimine divisores en el home header.

--- lib/src/widgets/home_header.dart
+++ lib/src/widgets/home_header.dart
@@ -9,25 +9,28 @@ Widget homeHeader(UserProvider userState) {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          flex: 4,
+          flex: 10,
           child: Row(
             children: <Widget>[
               Spacer(flex: 1),
               Expanded(
                 flex: 3,
-                child: Column(
-                  children: <Widget>[
-                    Spacer(
-                      flex: 1,
-                    ),
-                    Expanded(
-                      flex: 12,
-                      child: AvatarPicture(userState.user.avatarUrl),
-                    ),
-                    Spacer(
-                      flex: 1,
-                    ),
-                  ],
+                child: Container(
+                  color: Colors.red,
+                  child: Column(
+                    children: <Widget>[
+                      Spacer(
+                        flex: 1,
+                      ),
+                      Expanded(
+                        flex: 12,
+                        child: AvatarPicture(userState.user.avatarUrl),
+                      ),
+                      Spacer(
+                        flex: 1,
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Spacer(flex: 1),
@@ -70,8 +73,9 @@ Widget homeHeader(UserProvider userState) {
             ],
           ),
         ),
+        Spacer(flex: 1),
         Expanded(
-          flex: 2,
+          flex:6,
           child: Container(
             child: Row(
               children: <Widget>[
@@ -161,47 +165,41 @@ Widget homeHeader(UserProvider userState) {
             ),
           ),
         ),
+        Spacer(flex: 1),
         Expanded(
-          flex: 2,
+          flex: 6,
           child: Container(
             child: Row(
               children: <Widget>[
                 Expanded(
                   flex: 4,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        right: BorderSide(color: Colors.grey),
-                      ),
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 2,
-                          child: Text(
-                            userState.user.repositoryCount.toString(),
-                            textScaleFactor: 0.8,
-                            style: TextStyle(
-                              fontFamily: 'BalooDa',
-                              fontSize: 25,
-                              color: Colors.white,
-                            ),
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          userState.user.repositoryCount.toString(),
+                          textScaleFactor: 0.8,
+                          style: TextStyle(
+                            fontFamily: 'BalooDa',
+                            fontSize: 25,
+                            color: Colors.white,
                           ),
                         ),
-                        Flexible(
-                          flex: 2,
-                          child: Text(
-                            'Repository',
-                            textScaleFactor: 0.8,
-                            style: TextStyle(
-                              fontFamily: 'BalooDa',
-                              fontSize: 18,
-                              color: Colors.white,
-                            ),
+                      ),
+                      Flexible(
+                        flex: 2,
+                        child: Text(
+                          'Repository',
+                          textScaleFactor: 0.8,
+                          style: TextStyle(
+                            fontFamily: 'BalooDa',
+                            fontSize: 18,
+                            color: Colors.white,
                           ),
-                        )
-                      ],
-                    ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
                 Spacer(
@@ -209,42 +207,33 @@ Widget homeHeader(UserProvider userState) {
                 ),
                 Expanded(
                   flex: 4,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        right: BorderSide(
-                          color: Colors.grey,
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          userState.user.experience['total'].toString(),
+                          textScaleFactor: 0.8,
+                          style: TextStyle(
+                            fontFamily: 'BalooDa',
+                            fontSize: 25,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 2,
-                          child: Text(
-                            userState.user.experience['total'].toString(),
-                            textScaleFactor: 0.8,
-                            style: TextStyle(
-                              fontFamily: 'BalooDa',
-                              fontSize: 25,
-                              color: Colors.white,
-                            ),
+                      Flexible(
+                        flex: 2,
+                        child: Text(
+                          'Experience',
+                          textScaleFactor: 0.8,
+                          style: TextStyle(
+                            fontFamily: 'BalooDa',
+                            fontSize: 18,
+                            color: Colors.white,
                           ),
                         ),
-                        Flexible(
-                          flex: 2,
-                          child: Text(
-                            'Experience',
-                            textScaleFactor: 0.8,
-                            style: TextStyle(
-                              fontFamily: 'BalooDa',
-                              fontSize: 18,
-                              color: Colors.white,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 ),
                 Spacer(
